6cce556d88cbac8dc14b8cd14333d7b5a0413e4d
NASEEM A P <naseemalassampattil@gmail.com>
2022-03-05 11:42:27 +0530

    landmarks printing properly

--- fitness_trainer.py
+++ fitness_trainer.py
@@ -4,8 +4,17 @@ import time
 
 cap = cv2.VideoCapture('fitness videos/1.mp4')
 
+mp_pose = mp.solutions.pose
+pose = mp_pose.Pose()
+mp_draw = mp.solutions.drawing_utils
+
 while True:
     success, img = cap.read()
     img = cv2.resize(img, (740, 480))
+
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    result = pose.process(img_rgb)
+    print(result.pose_landmarks)
+
     cv2.imshow("Image", img)
     cv2.waitKey(1)
